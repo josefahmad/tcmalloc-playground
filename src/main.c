3a573e7c3e6c9bdce60f54a981258814be25d80c
Josef Ahmad <josef.ahmad@mongodb.com>
2019-08-03 22:29:08 +0200

    Call into tc_malloc

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
+#include "gperftools/malloc_hook_c.h"
+#include "gperftools/malloc_extension_c.h"
+#include "gperftools/tcmalloc.h"
+
 int main() {
+	char *c = tc_malloc(1);
+	(void)c;
+
 	return 0;
 }
